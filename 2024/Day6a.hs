776c0d641589ec8c6611c63ac723415d85bd2164
Milko Poolstok <milko.burkard1997@gmail.com>
2024-12-08 15:15:56 +0100

    Solved Day6a
    commit a095925be1a3083c065849b26de3628f82508542
    Author: Milko Poolstok <milko.burkard1997@gmail.com>
    Date:   Sat Dec 7 12:12:57 2024 +0100
    
        solved day6a
    
    commit 0294795b665b3c5de87a5a00f092ce3a34559f11
    Author: Milko Poolstok <milko.burkard1997@gmail.com>
    Date:   Sat Dec 7 11:39:23 2024 +0100
    
        create board

--- 2024/Day6a.hs
+++ 2024/Day6a.hs
@@ -35,22 +35,28 @@ move ORight = moveRight
 move OLeft  = moveLeft
 move ODown  = moveDown
 
-movePossible :: BoardPosition -> Orientation -> Bool
-movePossible position OUp    = row position > 1
-movePossible position ODown  = row position < size position
-movePossible position ORight = column position < size position
-movePossible position OLeft  = column position > 1
+outOfBoard :: BoardPosition -> Orientation -> Bool
+outOfBoard position OUp    = row position <= 1
+outOfBoard position ODown  = row position >= size position
+outOfBoard position ORight = column position >= size position
+outOfBoard position OLeft  = column position <= 1
 
-data Tile = Obstacle | Hidden | Seen | Start BoardPosition
-    deriving Show
+data Tile = Obstacle | Hidden | Seen | Start
+    deriving (Show,Eq)
+
+charToTile :: Char -> Tile
+charToTile '^' = Start
+charToTile '#' = Obstacle
+charToTile 'S' = Seen
+charToTile  _  = Hidden
 
 type Board = Map.Map BoardPosition Tile
 
 emptyBoard :: Integer -> Board
 emptyBoard n = Map.fromList [ (BoardPosition x y n, Hidden) | x <- [1..n], y <- [1..n]]
 
-editTileObstacle :: BoardPosition -> Tile -> Board -> Board
-editTileObstacle position tile = Map.adjust (const tile) position
+editTile :: (BoardPosition,Tile) -> Board -> Board
+editTile (position,tile) = Map.adjust (const tile) position
 
 checkForObstacle :: BoardPosition -> Orientation -> Board -> Bool
 checkForObstacle position orientation board = case board Map.! move orientation position of Obstacle -> True
@@ -60,17 +66,36 @@ data Guard = Guard {position :: BoardPosition, orientation :: Orientation}
     deriving Show
 
 moveGuard :: Board -> Guard -> Guard
-moveGuard board (Guard position orientation)
-    | movePossible position orientation = if checkForObstacle position orientation board then moveGuard board (Guard position (turnRight orientation)) else Guard (move orientation position) orientation
-    | otherwise                         = moveGuard board (Guard position (turnRight orientation))
+moveGuard board (Guard position orientation) = if checkForObstacle position orientation board 
+    then moveGuard board (Guard position (turnRight orientation)) 
+    else Guard (move orientation position) orientation
 
-createBoardFromInput :: String -> IO Board
+createBoardFromInput :: String -> IO (BoardPosition, Board)
 createBoardFromInput path = do
     handle <- openFile path ReadMode
     input  <- hGetContents handle
-    let size_board = length (lines input)
-    let rows = zip [1..size_board] (lines input)
-    return $ emptyBoard 0
+    let size_board = genericLength (lines input) :: Integer
+    let rows = zip [1..size_board] (lines input) :: [(Integer, String)]
+    let special_characters = concatMap (uncurry (keepSpecialCharacters size_board)) rows :: [(BoardPosition, Tile)]
+    let start_point = fst $ head $ filter (\x -> snd x == Start) special_characters  :: BoardPosition
+    return (start_point, createBoard size_board special_characters)
 
-keepSpecialCharacters :: Integer -> String -> [(Integer, Integer, String)]
-keepSpecialCharacters row string = [(row, column, s) | (column, s) <- zip [1..length string] string]
+keepSpecialCharacters :: Integer -> Integer -> String -> [(BoardPosition, Tile)]
+keepSpecialCharacters n row string = [
+    (BoardPosition row column n, charToTile s) |
+    (column, s) <- zip [1..(genericLength string)] string,
+     s /= '.']
+
+createBoard :: Integer -> [(BoardPosition, Tile)] -> Board
+createBoard size = foldr editTile (emptyBoard size)
+
+walkAroundBoard :: Guard -> Board -> Board
+walkAroundBoard guard board = if outOfBoard (position guard) (orientation guard) 
+    then editTile (position guard, Seen) board
+    else walkAroundBoard (moveGuard board guard) (editTile (position guard, Seen) board)
+
+main :: IO ()
+main = do 
+    (start, board) <- createBoardFromInput "Input/InputDay6.txt"
+    let final_board = walkAroundBoard (Guard start OUp) board :: Board
+    print $ length [x | x <- Map.elems final_board, x == Seen]
